3bbc9dd3a35480acba373cafb2b06e61a006988e
jeffb <jeffb@LAPTOP-DQFQ3O1Q.Home>
2016-11-07 17:45:28 -0600

    updating StereoExtended_MyStereo

--- ACA2016/src/edu/uca/aca2016/stereo/jeffbanksz4l/StereoRunner.java
+++ ACA2016/src/edu/uca/aca2016/stereo/jeffbanksz4l/StereoRunner.java
@@ -8,8 +8,7 @@ package edu.uca.aca2016.stereo.jeffbanksz4l;
 import java.io.IOException;
 import java.nio.file.Path;
 import java.nio.file.Paths;
-import java.util.ArrayList;
-import java.util.List;
+
 
 /**
  *
@@ -22,35 +21,37 @@ public class StereoRunner {
      */
     public static void main(String[] args) throws IOException {
         MyStereo myTracks = new MyStereo();
-        
+
         Path f = Paths.get("C:\\Users\\jeffb\\Desktop\\Banks_MyPlayList.txt");
         myTracks.loadTrackList(f.toFile());
-        
+
 //        List<String> trackListSource = new ArrayList<trackListSource>;
-        
-        myTracks.loadUSB();
-        System.out.println("Number of Tracks: " + myTracks.totalTrackCount());
-        System.out.println("Loaded: " + myTracks.isUSBLoaded());
-        System.out.println("Current Tack Number: " + myTracks.currentTrackNumber());
+//        myTracks.loadUSB();
+//        System.out.println("Number of Tracks: " + myTracks.totalTrackCount());
+//        System.out.println("Loaded: " + myTracks.isUSBLoaded());
+//        System.out.println("Current Track Number: " + myTracks.currentTrackNumber());
+//        System.out.println("Current Track Name: " + myTracks.getCurrentTrackFileName);
 //        System.out.println("Is Playing: " + myTracks.isPlaying());
 
 //        myTracks.isUSBLoaded();
 //        System.out.println("Loaded: " + myTracks.isUSBLoaded());
-
 //        myTracks.unloadUSB();
 //        System.out.println("Unload USB: " + myTracks.isUSBLoaded());
 //        System.out.println("Total Track Count: " + myTracks.totalTrackCount());
-
 //        myTracks.currentTrackNumber();
 //        System.out.println("Current Track Number: " + myTracks.currentTrackNumber());
-        
 //        myTracks.totalTrackCount();
 //        System.out.println("Total Track Count: " + myTracks.totalTrackCount());
+        myTracks.getTrackList();
+        System.out.println("Current Track List: " + myTracks.getTrackList());
 
         myTracks.nextTrack();
         System.out.println("Next Track: " + myTracks.currentTrackNumber());
-        
-        
+
+//        File trackListSource = null;
+//        myTracks.loadTrackList(trackListSource);
+//        System.out.println("Here is the current Track List: " + myTracks.loadTrackList(trackListSource));
+        myTracks.currentTrackNumber();
 
     }
 
