e1008f5d5a77e1c593055ded6ff150322e85abf7
calho <calho@LAPTOP-243QP7S1.philander.edu>
2016-12-15 18:31:23 -0600

    Merge Calhoun512-005 into calhoun512-006

--- ChinookManager/src/java/edu/uca/aca2016/web/Calhoun512/ChinookGenreMVC.java
+++ ChinookManager/src/java/edu/uca/aca2016/web/Calhoun512/ChinookGenreMVC.java
@@ -10,16 +10,21 @@ import edu.uca.aca2016.chinook.calhoun512.ChinookGenreManager;
 import java.io.IOException;
 import java.sql.SQLException;
 import java.util.HashMap;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+
+
 
 /**
  *
  * @author calho
  */
 public class ChinookGenreMVC extends HttpServlet{
+
     
     /**
      *
@@ -28,55 +33,65 @@ public class ChinookGenreMVC extends HttpServlet{
      * @throws ServletException
      * @throws IOException
      * @throws java.sql.SQLException
+     
      */
+    
     @Override
-    protected void doGet(HTTPServletRequest request, HTTPServletResponse response) throws ServletException, IOException, SQLException{
-        response.setContentType("text/html;charset=UTF-8");
-        
-        String mid = request.getParameter("mid");
-        String dothis = request.getParameter("do");
-        String message = "";
-        
-        if (mid == null || mid.isEmpty()) {
-            message = "";
+    protected void doGet(HttpServletRequest request,HttpServletResponse response) throws ServletException, IOException{ 
+        try {
+            response.setContentType("text/html;charset=UTF-8");
+            
+            String mid = request.getParameter("mid");
+            String dothis = request.getParameter("do");
+            String message = "";
+            
+            if (mid == null || mid.isEmpty()) {
+                message = "";
+            }
+            else if (mid.equals("100")) {
+                message = "Genre was added to database";
+            }
+            else if (mid.equals("101")) {
+                message = "Genre was not added, name value was empty or incorrect.";
+            }
+            else if (mid.equals("200")) {
+                message = "Genre was updated";
+            }
+            else if (mid.equals("201")) {
+                message = "Genre was not updated, name value was empty or incorrect or ID was missing.";
+            }
+            else if (mid.equals("300")) {
+                message = "Genre was deleted";
+            }
+            else if (mid.equals("301")) {
+                message = "Genre was not deleted";
+            }
+            
+            ChinookGenreManager cgm = new ChinookGenreManager();
+            HashMap<Integer, String> genres = cgm.getGenres();
+            
+            request.setAttribute("genres", genres);
+            request.setAttribute("dothis", dothis);
+            request.setAttribute("message", message);
+            
+            String id = request.getParameter("id");
+            
+            if (id != null && !id.isEmpty()) {
+                try {
+                    request.setAttribute("id", id);
+                    request.setAttribute("genre_name", cgm.getGenreName(Integer.parseInt(id)));
+                } catch (SQLException ex) {
+                    Logger.getLogger(ChinookGenreMVC.class.getName()).log(Level.SEVERE, null, ex);
+                }
+            }
+            
+            RequestDispatcher rd = request.getRequestDispatcher("/CalhounChinookGenreJSP.jsp");
+            rd.include(request, response);
+            
+            cgm.close();
+        } catch (SQLException ex) {
+            Logger.getLogger(ChinookGenreMVC.class.getName()).log(Level.SEVERE, null, ex);
         }
-        else if (mid.equals("100")) {
-            message = "Genre was added to database";
-        }
-        else if (mid.equals("101")) {
-            message = "Genre was not added, name value was empty or incorrect.";
-        }
-        else if (mid.equals("200")) {
-            message = "Genre was updated";
-        }
-        else if (mid.equals("201")) {
-            message = "Genre was not updated, name value was empty or incorrect or ID was missing.";
-        }
-        else if (mid.equals("300")) {
-            message = "Genre was deleted";
-        }
-        else if (mid.equals("301")) {
-            message = "Genre was not deleted";
-        }
-        
-        ChinookGenreManager cgm = new ChinookGenreManager();
-        HashMap<Integer, String> genres = cgm.getGenres();
-        
-        request.setAttribute("genres", genres);
-        request.setAttribute("dothis", dothis);
-        request.setAttribute("message", message);
-        
-        String id = request.getParameter("id");
-        
-        if (id != null && !id.isEmpty()) {
-            request.setAttribute("id", id);
-            request.setAttribute("genre_name", cgm.getGenreName(Integer.parseInt(id)));
-        }
-        
-        RequestDispatcher rd = request.getRequestDispatcher("/CalhounChinookGenreJSP.jsp");
-        rd.include(request, response);
-        
-        cgm.close();
         
     }
     
@@ -95,33 +110,45 @@ public class ChinookGenreMVC extends HttpServlet{
                     mid = "101";
                 }
                 else {
-                    cgm.addGenre(name);
-                    mid = "100";
+                    try {
+                        cgm.addGenre(name);
+                        mid = "100";
+                    } catch (SQLException ex) {
+                        Logger.getLogger(ChinookGenreMVC.class.getName()).log(Level.SEVERE, null, ex);
+                    }
                 }
             }
             else if (action.equals("Edit")) {
-                String name = request.getParameter("name");
-                int id = Integer.parseInt(request.getParameter("id"));
-                
-                boolean ret = cgm.updateGenre(id, name);
-                
-                if (ret) {
-                    mid = "200";
-                }
-                else {
-                    mid = "201";
+                try {
+                    String name = request.getParameter("name");
+                    int id = Integer.parseInt(request.getParameter("id"));
+                    
+                    boolean ret = cgm.updateGenre(id, name);
+                    
+                    if (ret) {
+                        mid = "200";
+                    }
+                    else {
+                        mid = "201";
+                    }
+                } catch (SQLException ex) {
+                    Logger.getLogger(ChinookGenreMVC.class.getName()).log(Level.SEVERE, null, ex);
                 }
             }
             else if (action.equals("Delete")) {
-                int id = Integer.parseInt(request.getParameter("id"));
-                
-                boolean ret = cgm.deleteGenre(id);
-                
-                if (ret) {
-                    mid = "300";
-                }
-                else {
-                    mid = "301";
+                try {
+                    int id = Integer.parseInt(request.getParameter("id"));
+                    
+                    boolean ret = cgm.deleteGenre(id);
+                    
+                    if (ret) {
+                        mid = "300";
+                    }
+                    else {
+                        mid = "301";
+                    }
+                } catch (SQLException ex) {
+                    Logger.getLogger(ChinookGenreMVC.class.getName()).log(Level.SEVERE, null, ex);
                 }
             }
         }
